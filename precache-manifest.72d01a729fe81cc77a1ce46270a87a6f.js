441fab2f6d2262c210e392bec2c20e95f567a2fb
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-02-20 22:51:15 -0500

    Updates

---
+++ precache-manifest.72d01a729fe81cc77a1ce46270a87a6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8955205369e767b9a78d3f415809137a",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "1ee5c3b3007cae69e772",
+    "url": "/face-recognition-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dade6212509663f9a1c8",
+    "url": "/face-recognition-app/static/css/main.66e93c0b.chunk.css"
+  },
+  {
+    "revision": "1ee5c3b3007cae69e772",
+    "url": "/face-recognition-app/static/js/2.99dbdf51.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face-recognition-app/static/js/2.99dbdf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dade6212509663f9a1c8",
+    "url": "/face-recognition-app/static/js/main.1b57fbaf.chunk.js"
+  },
+  {
+    "revision": "214d99cca5e47214cb55",
+    "url": "/face-recognition-app/static/js/runtime-main.a7b829c7.js"
+  }
+]);
